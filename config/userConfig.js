32189f398ff97e3ed523c253dadba4d9c032fa72
zihong <sxliuliang@163.com>
2016-09-01 09:31:29 +0800

    Update userConfig.js

--- config/userConfig.js
+++ config/userConfig.js
@@ -1,17 +1,11 @@
 module.exports =  {
 	'superUser':{
-		id:'1471834357181',
-		password:'1234567890',
-		project:['daoliu-wddk','cfq-crm','qiangui','daoliu-xyk','daoliu-xfd']
+		
 	},
 	'daoliu': {
-		id: '1471834194407',
-		password: '123456',
-		project:['daoliu-wddk','daoliu-xyk','daoliu-xfd']
+	
 	},
 	'chedai': {
-		id: '1471834223483',
-		password: '654321',
-		project:['cfq-crm']
+		
 	}
 }
